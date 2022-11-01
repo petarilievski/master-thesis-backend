b6cf67e5d6795905fa32179f522c771f786e6043
Petar <petar@producercloud.com>
2022-11-01 01:28:47 +0100

    fixes

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymcprotocol import Type1E
+import types
+
 app = Flask(__name__)
 CORS(app)
+
+pymc1e = Type1E()
+
+pymc = pymc1e.connect('192.168.110.24', 1025)
 
 sensors = {
     "M1": False,
@@ -20,8 +26,8 @@ def update_buttons():
 
 
 @app.route('/api/update-sensors', methods=['POST'])
-def update_buttons():
+def update_sensors():
     sensor = request.json['sensor']
     sensors[sensor] = not sensors[sensor]
 
-    return jsonify({sensor})
+    return jsonify(sensors)
